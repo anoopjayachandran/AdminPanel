3295cf501038c25611432b644202138929d8ead2
anujayachand <anujayachand@gmail.com>
2018-03-25 22:31:42 +0530

    init

---
+++ src/main/java/com/ap/view/MainView.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ap.view;
+
+import javax.faces.bean.ViewScoped;
+import javax.inject.Named;
+
+
+/**
+ *
+ * @author java-2
+ */
+@Named(value = "main")
+@ViewScoped
+public class MainView {
+
+    /**
+     * Creates a new instance of MainView
+     */
+    public MainView() {
+    }
+    
+}
